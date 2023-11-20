e7c06f3aabbf0451ff0dc3da530bea542e35b865
daehoon.kim <hoonpig@nate.com>
2023-11-20 22:43:05 +0900

    vue 리팩토링

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodoItems="addOneItem"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></TodoList>
+    <TodoFooter v-on:clearAll ="clearAllItems"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoFooter from './components/TodoFooter.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+
+
+/*
+var my_cpm = {
+  template : '<div>components</div>'
+}
+new Vue ({
+  el : '',
+  components :{
+    'mycpm' : my_cpm
+  }
+})
+*/
+
+export default {
+  data : function(){
+    return {
+      todoItems : []
+    }
+  }
+  , components : {
+    'TodoHeader' : TodoHeader 
+    , 'TodoFooter' : TodoFooter
+    , 'TodoInput' : TodoInput
+    , 'TodoList' : TodoList
+  }
+  , created :function(){
+    if(localStorage.length>0){
+      for(let i = 0 ; i < localStorage.length ; i++){
+        console.log(localStorage.key(i));
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){          
+          this.todoItems.push( JSON.parse(localStorage.getItem( localStorage.key(i))) );
+        }
+      }
+    }
+  }, methods :{
+    addOneItem : function (todoItem){
+      let obj = {completed : false, item : todoItem}
+      localStorage.setItem(todoItem , JSON.stringify(obj));
+      this.todoItems.push(obj);
+    }
+    , removeOneItem : function( todoItem, index){
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index,1);
+    }
+    , toggleOneItem : function(todoItem, index){
+        console.log('remove : ' , index , " value : " , todoItem) ;
+        this.todoItems[index].completed = !this.todoItems[index].completed
+        localStorage.removeItem(todoItem);
+        localStorage.setItem(todoItem.item,JSON.stringify(todoItem));
+    }
+    , clearAllItems : function(){
+      localStorage.clear();
+      this.todoItems = [];
+    }
+  }
+
+}
+</script>
+
+<style>
+ body {
+   text-align: center;
+   background-color: #f6f6f6;
+ }
+
+ input {
+   border-style: groove;
+   width : 200px;
+ }
+
+ button{
+   border-style: groove;
+ }
+
+ .shadow{
+   box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+ }
+</style>
